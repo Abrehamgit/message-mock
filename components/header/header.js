816f09a62c8fc6caac714d9fc0cbd18882fca6c5
Abrehamgit <gezahegnabereham@gmail.com>
2019-03-11 13:08:57 -0700

    header alignment to left

--- components/header/header.js
+++ components/header/header.js
@@ -3,14 +3,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledHeader = styled.div`
-	width: 100%;
+	width: 67%;
+	@media (max-width: 600px) {
+		width: 100%;
+		padding-left: 1rem;
+	}
 	border-bottom: solid rgba(233, 233, 233, 0.5) 2px;
-	padding-left: 1rem;
 	margin-bottom: 1.6rem;
 	font-size: 1.6rem;
 	font-weight: 600;
 	display: flex;
-	justify-content: center;
+	justify-content: flex-start;
 `;
 
 const Header = props => <StyledHeader> Messages </StyledHeader>;
